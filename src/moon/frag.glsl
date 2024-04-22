846e451723711dbbf87857cf48f78e9f248032d9
chantep <541149506@qq.com>
2024-04-22 18:39:33 +0800

    fix: moon

--- src/moon/frag.glsl
+++ src/moon/frag.glsl
@@ -22,6 +22,16 @@ vec2 getLatLon(vec2 pos,vec2 center,float r,float offsetLong){
     return vec2(lat/PI*2.,fract(lon/2./PI));
 }
 
+vec3 getXYZ(vec2 latlon,float r){
+    float r1=cos(latlon.x*PI/2.)*r;
+    
+    return vec3(
+        r1*cos(latlon.y*2.*PI),
+        r*sin(latlon.x*PI/2.),
+        r1*sin(latlon.y*2.*PI)
+    );
+}
+
 vec2 moonCoordFromLatlon(vec2 latlon){
     float x=latlon.y;
     float y=latlon.x/2.+.5;
@@ -29,12 +39,67 @@ vec2 moonCoordFromLatlon(vec2 latlon){
     return vec2(x,y);
 }
 
-vec4 moonShadow(vec2 latlon,float offset){
-    float scale=6.;
-    float shadowValue=cos((offset-latlon.y)*2.*PI)*scale/2.+.5;
-    shadowValue = clamp(shadowValue, 0., 1.2);
+// vec4 moonLight(vec2 latlon,float offset, vec4 light){
+    //     float scale=6.;
+    //     float shadowValue=cos((offset-latlon.y)*2.*PI)*scale/2.+.5;
+    //     shadowValue=clamp(shadowValue,0.,1.2);
     
-    return vec4(shadowValue,shadowValue,shadowValue,1.);
+    //     return vec4(shadowValue,shadowValue,shadowValue,1.);
+// }
+
+mat3 getRotationMatrixZ(float theta){
+    float c=cos(theta);// GLSL 中的cos函数返回弧度的余弦值
+    float s=sin(theta);// GLSL 中的sin函数返回弧度的正弦值
+    
+    // 创建绕z轴的旋转矩阵
+    mat3 rotationMatrix=mat3(
+        c,-s,0,
+        0,0,1,
+        s,c,0
+    );
+    
+    return rotationMatrix;
+}
+// 计算旋转后的坐标
+vec3 getRotatedPoint(float r,float degreeOffset,float t){
+    vec3 targetA=vec3(
+        r*cos(t),
+        r*sin(t),
+        0.
+    );
+    mat3 R=getRotationMatrixZ(degreeOffset);
+    return R*targetA;
+}
+
+// 计算两个向量之间的夹角（弧度值）
+float calculateAngle(vec3 vecA,vec3 vecB){
+    // 计算两个向量的点乘
+    float dotProduct=dot(vecA,vecB);
+    // 计算两个向量的模长
+    float lengthA=length(vecA);
+    float lengthB=length(vecB);
+    // 计算夹角的余弦值
+    float cosAngle=dotProduct/(lengthA*lengthB);
+    // 确保余弦值在[-1, 1]的范围内，防止由于精度问题导致的计算错误
+    cosAngle=clamp(cosAngle,-1.,1.);
+    // 计算夹角（弧度值）
+    float angle=acos(cosAngle);
+    return angle;
+}
+
+vec4 moonLight(vec2 latlon,float t,vec4 lightColor){
+    float lightDistance=12.;
+    float degreeOffset=PI/180.*-12.;
+    // float degreeOffset = 0.;
+    vec3 lightPos=getRotatedPoint(lightDistance,degreeOffset,t);
+    
+    vec3 pos=getXYZ(latlon,1.);
+    
+    float angle=calculateAngle(pos,lightPos);
+    
+    float shadowValue=max(0., cos(angle));
+    
+    return vec4(shadowValue,shadowValue,shadowValue,1.)*lightColor;
 }
 
 void main(){
@@ -44,22 +109,24 @@ void main(){
     vec2 center=vec2(u_resolution.xy)/2.;
     float r=min(u_resolution.x,u_resolution.y)/1.5/2.;
     
+    vec4 lightColor=vec4(1.,.9765,.8706,1.);
+    vec4 spaceColor=vec4(.051,0.,1.,1.) * .08;
+    
     vec2 vec=pos/u_resolution;
     
     if(distance(pos,center)<=r){
-        vec2 latlon=getLatLon(pos,center,r,1.8+u_time*SPEED);
+        vec2 latlon=getLatLon(pos,center,r,4.*(1.8+u_time*SPEED));
         vec2 moonCoord=moonCoordFromLatlon(latlon);
-        // pixel.r=abs(latlon.x);
-        // pixel.b=latlon.y;
-        // pixel.a=1.;
+        
         pixel=texture2D(iChannel0,moonCoord);
         
-        float shadowTime=u_time*SPEED/2.;
-        vec4 shadow=moonShadow(latlon,shadowTime);
-        pixel=pixel*shadow*vec4(1.,.9765,.8706,1.);
+        float lightT=u_time*SPEED*5.;
+        vec4 light=moonLight(latlon,lightT,lightColor*1.7);
+        
+        pixel=pixel*light+spaceColor;
         // pixel=shadow;
         
-        // pixel=moonShadow(vec, (u_time/SPEED));
+        // pixel=moonLight(vec, (u_time/SPEED));
         // pixel.b=latlon.y;
         // pixel.r=fract(u_time);
         pixel.a=1.;
